4f31eee759500a2de5fd1e6233919b4f6ea5cc4f
jimmy <vphu456@gmail.com>
2022-11-01 18:31:52 +0700

    update file

---
+++ pages/api/index.js
@@ -0,0 +1,161 @@
+import jimmy from "../assets/jimmy.jpg";
+import shopee from "../assets/shopee.png";
+import quanlinhanvien from "../assets/quanlinhanvien.png";
+import sassweb from "../assets/sassweb.png";
+import samar from "../assets/samar.png";
+import hoobank from "../assets/hoobank.png";
+import bootstrap from "../assets/bootstrap.png";
+import html from "../assets/html5.svg";
+import css from "../assets/css-3.svg";
+import bs from "../assets/bootstrap-4.svg";
+import sass from "../assets/sass-1.svg";
+import tail from "../assets/tailwindcss.svg";
+import js from "../assets/JavaScript_logo.svg";
+import ajax from "../assets/ajax.png";
+import axios from "../assets/axios.png";
+import github from "../assets/github.png";
+import es6 from "../assets/es6.svg";
+
+export {
+  jimmy,
+  shopee,
+  quanlinhanvien,
+  sassweb,
+  samar,
+  hoobank,
+  bootstrap,
+  html,
+  css,
+  bs,
+  sass,
+  tail,
+  js,
+  ajax,
+  axios,
+  github,
+  es6,
+};
+
+export const project = [
+  {
+    id: 1,
+    name: "Shopee",
+    desc: "web product shoe shop get api ",
+    tech: "HTML, SCSS, JS ,Axios, Postman",
+    img: shopee,
+    linkGit: "https://github.com/JimmyNguyen22/shopeeJs",
+    linkDemo: "https://jimmynguyen22.github.io/shop/shopee.html",
+    linkYouTube: (
+      <iframe
+        width="100%"
+        height="400"
+        src="https://www.youtube.com/embed/Kv5NHwKOmzI"
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen
+      ></iframe>
+    ),
+  },
+  {
+    id: 2,
+    name: "Staff Management",
+    desc: "Employee Payroll Calculate Program and CRUD",
+    tech: "HTML/CSS/JS ,Bootstrap",
+    img: quanlinhanvien,
+    linkGit: "https://github.com/JimmyNguyen22/QuanLyNhanVien_CRUD",
+    linkDemo: "https://quanlynhanvien-crud.surge.sh/",
+    linkYouTube: (
+      <iframe
+        width="560"
+        height="315"
+        src="https://www.youtube.com/embed/hbeDPYq-_Kg"
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen
+      ></iframe>
+    ),
+  },
+  {
+    id: 3,
+    name: "E-learning landing page",
+    desc: "Build layout professional with sass and rule BEM",
+    tech: "SCSS ,JS , OWL carousel, Bootstrap, Swiper",
+    img: sassweb,
+    linkGit: "https://github.com/JimmyNguyen22/SASS_landingPage",
+    linkDemo: "https://jimmynguyen22.github.io/SASS_landingPage/",
+    linkYouTube: (
+      <iframe
+        width="560"
+        height="315"
+        src="https://www.youtube.com/embed/qskxe6No2qY"
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen
+      ></iframe>
+    ),
+  },
+  {
+    id: 4,
+    name: "Samar",
+    desc: "Web landing page with modern UI/UX ",
+    tech: "HTML/CSS/JS , OWL carousel, Animate, Wowjs, Swiper",
+    img: samar,
+    linkGit: "https://github.com/JimmyNguyen22/shopeeJs",
+    linkDemo: "https://jimmynguyen22.github.io/shop/shopee.html",
+    linkYouTube: (
+      <iframe
+        width="560"
+        height="315"
+        src="https://www.youtube.com/embed/d3ACgRob4h8"
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen
+      ></iframe>
+    ),
+  },
+  {
+    id: 5,
+    name: "Landing page",
+    desc: "Web landing page form layout design",
+    tech: "HTML/CSS/JS , Bootstrap",
+    img: bootstrap,
+    linkGit: "https://github.com/JimmyNguyen22/Capstone_landingPage",
+    linkDemo:
+      "https://jimmynguyen22.github.io/Capstone_landingPage/capstone.html",
+    linkYouTube: (
+      <iframe
+        width="560"
+        height="315"
+        src="https://www.youtube.com/embed/fFHA8xBNblA"
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen
+      ></iframe>
+    ),
+  },
+  {
+    id: 6,
+    name: "Hoobank",
+    desc: "Web fully responsive with a modern UI/UX",
+    tech: "Base Reactjs, ViteJs, Tailwindcss",
+    img: hoobank,
+    linkGit: "https://github.com/JimmyNguyen22/landingPage_reactjs",
+    linkDemo: "https://hoobank-ladingpage.surge.sh/",
+    linkYouTube: (
+      <iframe
+        width="560"
+        height="315"
+        src="https://www.youtube.com/embed/tRN89QcYhU0"
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen
+      ></iframe>
+    ),
+  },
+];
--- pages/index.jsx
+++ pages/index.jsx
@@ -7,149 +7,22 @@ import {
   AiOutlinePlayCircle,
 } from "react-icons/ai";
 import Image from "next/image";
-import jimmy from "../public/jimmy.jpg";
-import shopee from "../public/shopee.png";
-import quanlinhanvien from "../public/quanlinhanvien.png";
-import sassweb from "../public/sassweb.png";
-import samar from "../public/samar.png";
-import hoobank from "../public/hoobank.png";
-import bootstrap from "../public/bootstrap.png";
-import html from "../public/html5.svg";
-import css from "../public/css-3.svg";
-import bs from "../public/bootstrap-4.svg";
-import sass from "../public/sass-1.svg";
-import tail from "../public/tailwindcss.svg";
-import js from "../public/JavaScript_logo.svg";
-import ajax from "../public/ajax.png";
-import axios from "../public/axios.png";
-import github from "../public/github.png";
-import es6 from "../public/es6.svg";
+import {
+  jimmy,
+  html,
+  css,
+  bs,
+  sass,
+  tail,
+  js,
+  ajax,
+  axios,
+  github,
+  es6,
+} from "./api";
+import { project } from "./api";
 import { Fragment, useState } from "react";
 import { Modal } from "../components/Modal";
-
-const project = [
-  {
-    id: 1,
-    name: "Shopee",
-    desc: "web product shoe shop get api ",
-    tech: "HTML, SCSS, JS ,Axios, Postman",
-    img: shopee,
-    linkGit: "https://github.com/JimmyNguyen22/shopeeJs",
-    linkDemo: "https://jimmynguyen22.github.io/shop/shopee.html",
-    linkYouTube: (
-      <iframe
-        width="100%"
-        height="400"
-        src="https://www.youtube.com/embed/Kv5NHwKOmzI"
-        title="YouTube video player"
-        frameborder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        allowfullscreen
-      ></iframe>
-    ),
-  },
-  {
-    id: 2,
-    name: "Staff Management",
-    desc: "Employee Payroll Calculate Program and CRUD",
-    tech: "HTML/CSS/JS ,Bootstrap",
-    img: quanlinhanvien,
-    linkGit: "https://github.com/JimmyNguyen22/QuanLyNhanVien_CRUD",
-    linkDemo: "https://quanlynhanvien-crud.surge.sh/",
-    linkYouTube: (
-      <iframe
-        width="560"
-        height="315"
-        src="https://www.youtube.com/embed/hbeDPYq-_Kg"
-        title="YouTube video player"
-        frameborder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        allowfullscreen
-      ></iframe>
-    ),
-  },
-  {
-    id: 3,
-    name: "E-learning landing page",
-    desc: "Build layout professional with sass and rule BEM",
-    tech: "SCSS ,JS , OWL carousel, Bootstrap, Swiper",
-    img: sassweb,
-    linkGit: "https://github.com/JimmyNguyen22/SASS_landingPage",
-    linkDemo: "https://jimmynguyen22.github.io/SASS_landingPage/",
-    linkYouTube: (
-      <iframe
-        width="560"
-        height="315"
-        src="https://www.youtube.com/embed/qskxe6No2qY"
-        title="YouTube video player"
-        frameborder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        allowfullscreen
-      ></iframe>
-    ),
-  },
-  {
-    id: 4,
-    name: "Samar",
-    desc: "Web landing page with modern UI/UX ",
-    tech: "HTML/CSS/JS , OWL carousel, Animate, Wowjs, Swiper",
-    img: samar,
-    linkGit: "https://github.com/JimmyNguyen22/shopeeJs",
-    linkDemo: "https://jimmynguyen22.github.io/shop/shopee.html",
-    linkYouTube: (
-      <iframe
-        width="560"
-        height="315"
-        src="https://www.youtube.com/embed/d3ACgRob4h8"
-        title="YouTube video player"
-        frameborder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        allowfullscreen
-      ></iframe>
-    ),
-  },
-  {
-    id: 5,
-    name: "Landing page",
-    desc: "Web landing page form layout design",
-    tech: "HTML/CSS/JS , Bootstrap",
-    img: bootstrap,
-    linkGit: "https://github.com/JimmyNguyen22/Capstone_landingPage",
-    linkDemo:
-      "https://jimmynguyen22.github.io/Capstone_landingPage/capstone.html",
-    linkYouTube: (
-      <iframe
-        width="560"
-        height="315"
-        src="https://www.youtube.com/embed/fFHA8xBNblA"
-        title="YouTube video player"
-        frameborder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        allowfullscreen
-      ></iframe>
-    ),
-  },
-  {
-    id: 6,
-    name: "Hoobank",
-    desc: "Web fully responsive with a modern UI/UX",
-    tech: "Base Reactjs, ViteJs, Tailwindcss",
-    img: hoobank,
-    linkGit: "https://github.com/JimmyNguyen22/landingPage_reactjs",
-    linkDemo: "https://hoobank-ladingpage.surge.sh/",
-    linkYouTube: (
-      <iframe
-        width="560"
-        height="315"
-        src="https://www.youtube.com/embed/tRN89QcYhU0"
-        title="YouTube video player"
-        frameborder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        allowfullscreen
-      ></iframe>
-    ),
-  },
-];
 
 export default function Home() {
   const [darkMode, setDarkMode] = useState(true);
@@ -357,14 +230,13 @@ export default function Home() {
         </main>
       </div>
       <Modal isVisible={modal} onClose={() => setModal(false)}>
-        {project.filter((item, index) => {
-          if (item.id === item[index])
-            return <div>{item.linkYouTube}</div>;
-        })}
-
-        {/* {project.map((item) => {
-           return <div key={item.id}>{item.linkYouTube}</div>;
+        {/* {project.filter((item, index) => {
+          if (item.id === item[index]) return <div>{item.linkYouTube}</div>;
         })} */}
+
+        {project.map((item) => {
+          return <div key={item.id}>{item.linkYouTube}</div>;
+        })}
       </Modal>
     </Fragment>
   );
