d658982ec43354c4cb8f16e60c3e143ffabafb69
jimmy <vphu456@gmail.com>
2022-12-12 19:11:35 +0700

    update

--- pages/api/index.js
+++ pages/api/index.js
@@ -1,10 +1,9 @@
 import jimmy from "../../assets/jimmy.jpg";
 import shopee from "../../assets/shopee.png";
-import quanlinhanvien from "../../assets/quanlinhanvien.png";
-import sassweb from "../../assets/sassweb.png";
-import samar from "../../assets/webcss.png";
+import christmas from "../../assets/christmas.png";
+import headphone from "../../assets/headphone.png";
+import lol from "../../assets/lol.png";
 import hoobank from "../../assets/hoobank.png";
-import bootstrap from "../../assets/bootstrap.png";
 import html from "../../assets/html5.svg";
 import css from "../../assets/css-3.svg";
 import bs from "../../assets/bootstrap-4.svg";
@@ -19,11 +18,10 @@ import es6 from "../../assets/es6.svg";
 export {
   jimmy,
   shopee,
-  quanlinhanvien,
-  sassweb,
-  samar,
+  christmas,
+  headphone,
+  lol,
   hoobank,
-  bootstrap,
   html,
   css,
   bs,
@@ -39,12 +37,12 @@ export {
 export const project = [
   {
     id: 1,
-    name: "Shopee",
-    desc: "web product shoe shop get api ",
+    name: "Shoe Shop",
+    desc: "Shoes shop with layout clone shopee",
     tech: "HTML, SCSS, JS ,Axios, Postman",
     img: shopee,
-    linkGit: "https://github.com/JimmyNguyen22/shopeeJs",
-    linkDemo: "https://jimmynguyen22.github.io/shop/shopee.html",
+    linkGit: "https://github.com/JimmyNguyen22/shoe_shopJS",
+    linkDemo: "https://jimmynguyen22.github.io/shoe_shopJS/shopee.html",
     linkYouTube: (
       <iframe
         width="100%"
@@ -59,12 +57,12 @@ export const project = [
   },
   {
     id: 2,
-    name: "Staff Management",
-    desc: "Employee Payroll Calculate Program and CRUD",
-    tech: "HTML/CSS/JS ,Bootstrap",
-    img: quanlinhanvien,
-    linkGit: "https://github.com/JimmyNguyen22/QuanLyNhanVien_CRUD",
-    linkDemo: "https://quanlynhanvien-crud.surge.sh/",
+    name: "Christmas Responsive",
+    desc: "Beautiful web layout with modern UX/UI",
+    tech: "HTML/CSS/JS , GSAP",
+    img: christmas,
+    linkGit: "https://github.com/JimmyNguyen22/christmas-responsive",
+    linkDemo: "https://demo-christmas-responsive.netlify.app/",
     linkYouTube: (
       <iframe
         width="560"
@@ -79,12 +77,12 @@ export const project = [
   },
   {
     id: 3,
-    name: "E-learning landing page",
-    desc: "Build layout professional with sass and rule BEM",
-    tech: "SCSS ,JS , OWL carousel, Bootstrap, Swiper",
-    img: sassweb,
-    linkGit: "https://github.com/JimmyNguyen22/SASS_landingPage",
-    linkDemo: "https://jimmynguyen22.github.io/SASS_landingPage/",
+    name: "Headphones",
+    desc: "Beautiful headphone landing page",
+    tech: "HTML, CSS, JS",
+    img: headphone,
+    linkGit: "https://github.com/JimmyNguyen22/headphone-responsive",
+    linkDemo: "https://demo-headphones.netlify.app/",
     linkYouTube: (
       <iframe
         width="560"
@@ -99,12 +97,12 @@ export const project = [
   },
   {
     id: 4,
-    name: "Samar",
-    desc: "Web landing page with modern UI/UX ",
-    tech: "HTML/CSS/JS , OWL carousel, Animate, Wowjs, Swiper",
-    img: samar,
-    linkGit: "https://github.com/JimmyNguyen22/landingPage_Samar",
-    linkDemo: "https://jimmynguyen22.github.io/landingPage_Samar/samar.html",
+    name: "Game lading page",
+    desc: "Web landing page with smooth scroll and animation",
+    tech: "ReactJs, SASS",
+    img: lol,
+    linkGit: "https://github.com/JimmyNguyen22/lol_Reactjs",
+    linkDemo: "https://lol-landing-page.surge.sh/",
     linkYouTube: (
       <iframe
         width="560"
@@ -117,32 +115,12 @@ export const project = [
       ></iframe>
     ),
   },
+
   {
     id: 5,
-    name: "Landing page",
-    desc: "Web landing page form layout design",
-    tech: "HTML/CSS/JS , Bootstrap",
-    img: bootstrap,
-    linkGit: "https://github.com/JimmyNguyen22/Capstone_landingPage",
-    linkDemo:
-      "https://jimmynguyen22.github.io/Capstone_landingPage/capstone.html",
-    linkYouTube: (
-      <iframe
-        width="560"
-        height="315"
-        src="https://www.youtube.com/embed/fFHA8xBNblA"
-        title="YouTube video player"
-        frameborder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        allowfullscreen
-      ></iframe>
-    ),
-  },
-  {
-    id: 6,
     name: "Hoobank",
-    desc: "Web fully responsive with a modern UI/UX",
-    tech: "Base Reactjs, ViteJs, Tailwindcss",
+    desc: "Web full responsive with modern UI/UX",
+    tech: "Base ReactJs, Tailwindcss",
     img: hoobank,
     linkGit: "https://github.com/JimmyNguyen22/landingPage_reactjs",
     linkDemo: "https://hoobank-ladingpage.surge.sh/",
